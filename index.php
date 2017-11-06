fade2d0c88d2ed8f49dd9f9cd60a405a494a7d5a
James Read <contact@jread.com>
2017-11-06 19:22:28 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <?php
 $hostname = gethostname();
+
+function stringToColor($s) {                                                       
+    $crcVal = crc32($s);                                                           
+                                                                                   
+    $r = $crcVal & 0xff;                                                           
+    $g = $crcVal >> 2 & 0xff;                                                      
+    $b = $crcVal >> 3 & 0xff;                                                      
+                                                                                   
+    return "rgb($r, $g, $b)\n";                                                    
+} 
+
+$color = stringToColor($hostname);
+
 ?>
 <link rel = "stylesheet" type = "text/css" href = "style.css" />
 
-<body>
+<body style = "background-color: <?=$color ?>;">
     <h1>Hi! This is php-show-my-hostname!</h1>
     
     <p>My hostname is <strong><?=$hostname ?></strong>!</p>   
