e89ff9610d0e807240b070fb8a2b3e6ea4c577e8
Bobby DeVeaux <me@bobbyjason.co.uk>
2017-11-22 09:47:56 +0000

    adding contrasting text color

--- index.php
+++ index.php
@@ -8,17 +8,30 @@ function stringToColor($s) {
     $g = $crcVal >> 2 & 0xff;                                                      
     $b = $crcVal >> 3 & 0xff;                                                      
                                                                                    
-    return "rgb($r, $g, $b)";                                                    
+    return array($r, $g, $b);
+    
 } 
+
+//https://stackoverflow.com/questions/38063499/how-to-invert-rgb-hex-values-by-contrast-in-php
+function contrastColor($colorArray) {
+    $r = ($colorArray[0] < 128) ? 255 : 0;
+    $g = ($colorArray[1] < 128) ? 255 : 0;
+    $b = ($colorArray[2] < 128) ? 255 : 0;
+
+    return array($r, $g, $b);
+}
 
 $showGitHubLink = trim(strtoupper(getenv('SHOW_GITHUB_LINK'))) == "YES";
 
-$color = stringToColor($hostname);
+$colorArray = stringToColor($hostname);
+$contrastColor = contrastColor($colorArray);
+$color = "rgb($colorArray[0],$colorArray[1],$colorArray[2])";
+$text = "rgb($contrastColor[0],$contrastColor[1],$contrastColor[2])";
 
 ?>
 <link rel = "stylesheet" type = "text/css" href = "style.css" />
 
-<body style = "background-color: <?=$color ?>;">
+<body style = "background-color: <?=$color ?>;color: <?=$text ?>;">
     <h1>Hi! This is php-show-my-hostname!</h1>
     
     <p>My hostname is <strong><?=$hostname ?></strong>!</p>   
